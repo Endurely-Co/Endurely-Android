9dcc591720cf5757a86378310ede46e0d71949a9
Devmike01 <michaelgbenga91@gmail.com>
2025-05-14 14:23:54 +0100

    Removed useless codes

--- app/src/main/java/dev/gbenga/endurely/onboard/data/ApiEndpoint.kt
+++ app/src/main/java/dev/gbenga/endurely/onboard/data/ApiEndpoint.kt
@@ -1,28 +1,16 @@
 package dev.gbenga.endurely.onboard.data
 
 object ApiEndpoint {
-    const val blocalBaseUrl = "http://127.0.0.1:8000"
+    const val localBaseUrl = "http://127.0.0.1:8000"
     const val BASE_URL = "https://carbide-cairn-451020-k0.nw.r.appspot.com"
     const val ROUTINES = "routines"
     const val MEAL = "meal"
     const val USER_ROUTINES = "$ROUTINES/user"
     const val EXERCISES = "$ROUTINES/exercises"
     const val ADD_ROUTINES = "$ROUTINES/add"
-    const val MEAL_RECOMMENDATIONS = "$MEAL/recommendations"
     const val MEAL_PLAN = "$MEAL/plan"
     const val MEAL_NUTRIENT= "$MEAL/nutrient"
-    //const val DELETE_ROUTINE = "routines/19?routine=23cf0668153e4d18a4a4bc3ab0f7a0f9"
-    const val EXERCISES_CATEGORIES = "$ROUTINES/categories"
-    const val ROUTINES_ADD = "$ROUTINES/add"
-    const val ROUTINES_UPDATE = "$ROUTINES/update"
-    const val ROUTINES_DELETE = "$ROUTINES/delete"
-    const val ROUTINES_NUTRIENTS = "$ROUTINES/food/nutrients"
-    const val EXERCISES_CATEGORY_BY_ID = "$ROUTINES/exercises/category"
     const val ONBOARD = "/onboarding"
     const val signIn = "$ONBOARD/login"
     const val signUp = "$ONBOARD/create-account"
-    const val refreshToken = "$ONBOARD/refresh-token"
-    const val generateToken = "$ONBOARD/generate-otp"
-    const val validateOtp = "$ONBOARD/validate-otp"
 }
-//http://127.0.0.1:8000/onboarding/login
